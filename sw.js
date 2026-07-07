c8545305962ea753f09675e3d630f23ebc8a9360
nirvanadurgesh1994-netizen <nirvanadurgesh1994@gmail.com>
2026-07-08 01:06:04 +0400

    Bump cache for iPad optimized deployment

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nature-fit-ios-menu-v3';
+const CACHE_NAME = 'nature-fit-ios-menu-v4';
 const APP_SHELL = ['./','./index.html','./manifest.webmanifest','./icons/naturefit.svg'];
 
 self.addEventListener('install', event => {
